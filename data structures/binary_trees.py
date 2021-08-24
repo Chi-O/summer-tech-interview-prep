9595429a881a8800c158c72602aed63a2d2a9ac9
Chi-O <chioma.o3762@gmail.com>
2021-08-24 21:18:00 +0000

    updated traversal methods

--- data structures/binary_trees.py
+++ data structures/binary_trees.py
@@ -3,6 +3,9 @@ class Tree:
   def __init__(self, root):
     self.root = root
 
+  def add(self, data):
+    self.root.add_node(data)
+
 class Node:
   def __init__(self, data):
     self.data = data
@@ -13,19 +16,19 @@ class Node:
   # if data = node, return
   # if data < node, recurse to left and add data
   # if data > node, recurse to right and add data
-  def add(self, data):
+  def add_node(self, data):
     if self.data == data:
       return
     
-    if self.data < data:
+    if data < self.data:
       if self.left:
-        self.left.add(data)
+        self.left.add_node(data)
       else:
         self.left = Node(data)
     
-    if self.data > data:
+    if data > self.data:
       if self.right:
-        self.right.add(data)
+        self.right.add_node(data)
       else:
         self.right = Node(data)
 
@@ -44,10 +47,10 @@ class Node:
   # visit left, then right, then node
   def post_order(self):
     if self.left:
-      self.left.pre_order()
+      self.left.post_order()
     
     if self.right:
-      self.right.pre_order()
+      self.right.post_order()
 
     # visit node
     print(self.data)
@@ -55,19 +58,31 @@ class Node:
   # visit left, then node, then right
   def in_order(self):
     if self.left:
-      self.left.pre_order()
+      self.left.in_order()
 
     # visit node
     print(self.data)
     
     if self.right:
-      self.right.pre_order()
-
+      self.right.in_order()
 
 
 apple = Tree(Node(4))
-apple.root.add(5)
-apple.root.add(10)
-apple.root.add(76)
-apple.root.add(50)
+# apple.root.add(5)
+# apple.root.add(10)
+# apple.root.add(76)
+# apple.root.add(50)
+apple.add(6)
+apple.add(5)
+apple.add(50)
+apple.add(3)
+apple.add(13)
+
+print("in-order traversal:")
 apple.root.in_order()
+
+print("post-order traversal:")
+apple.root.post_order()
+
+print("pre-order traversal:")
+apple.root.pre_order()
