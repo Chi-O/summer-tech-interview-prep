e69f76b2e9b1e980a9ab9f10c9a917d5edbaa714
Chi-O <chioma.o3762@gmail.com>
2021-11-05 10:33:16 -0400

    solved kth largest element

--- mock interviews/kth_largest.py
+++ mock interviews/kth_largest.py
@@ -1,21 +1,31 @@
-from collections import Counter
+import heapq
+
+"""
+revised solution:
+
+(1) insert the first k elements into min heap
+(2) for arr[k to n-1]:
+        if arr[i] > root:
+          replace root with arr[i]
+          fix heap condition
+(3) k-th largest is the root
+"""
+def kth_largest(arr, K):
+  # base case
+  if not arr or (K > len(arr)):
+    return None
+
+  # build min-heap from first k elements in arr
+  pq = arr[0:K]
+  heapq.heapify(pq)
+
+  for i in range(K, len(arr)):
+    if arr[i] > pq[0]:
+      # replace root with element
+      heapq.heapreplace(pq, arr[i])
+  
+  # return the root of the heap
+  return pq[0]
 
 
-def kthlargest(nums, k): 
-    hashmap = Counter(nums)
-    currentsum = 0
-  # for i in list(hashmap.values()):
-    print(hashmap.keys())
-    print(hashmap.values())
-    # i = max(hashmap, key = int) 
-    # # if the value is < k 
-    # if hashmap[i] < k:
-    #   currentsum += hashmap[i]
-    #   hashmap.pop(i)
-    # 	i+=1
-    # if currentsum == k:
-    #   return i 
-
-
-case1 = [3,2,3,1,2,4,5,5,6]
-kthlargest(case1, 4)
+print(kth_largest([3,2,1,5,6,4], 2))
